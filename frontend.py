d837702c92f23f86f5048f0f12cd59def99f48da
kat143 <kat221414@gmail.com>
2025-04-27 12:10:19 -0400

    remove "raise child" thing

--- frontend.py
+++ frontend.py
@@ -11,10 +11,6 @@ reg_loaded = joblib.load("reg_model.pkl")
 st.title("Predicting College Tuition")
 st.write("Team Members: Lucas He, Aden Zhao, Kaitlyn Lu, Rahi Dasgupta")
 
-st.subheader("How will you raise your child?")
-
-childcare = st.radio("Level", options=["Daycare", "Stay-at Home Parent"])
-
 high_school_graduation_year = st.number_input("High School Graduation Year:", min_value=1900, value=2025)
 education_level = st.radio("Level", options=["Private", "public in-state", "public out-of-state"])
 
@@ -28,7 +24,7 @@ elif education_level == "public out-of-state":
 if st.button("Predict Cost"):
     tuition = estimate_tuition(program, int(high_school_graduation_year))
     # new_tuition = reg_loaded.predict() # TODO: need to format data properly
-    st.caption(f"{tuition}")
+    st.caption(f"Per semester, the approximate tuition is ${round(tuition, 2)} (adjusted for inflation).")
 
 
 # Note: the prediction doesn't work right now since we need to format the data properly before passing it into the model.
