c64a295ed7c1615290a1210fc3fbc9d43222f167
aden-zhao <adenzhao88@gmail.com>
2025-04-27 11:26:03 -0400

    new dataset for college salaries

---
+++ frontend.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from model import *
+from overall_tuition_model import *
+
+df = loadData()
+
+reg_loaded = joblib.load("reg_model.pkl")
+
+st.title("Predicting College Tuition")
+st.write("Team Members: Lucas He, Aden Zhao, Kaitlyn Liu, Rahi Dasgupta")
+
+st.subheader("How will you raise your child?")
+
+childcare = st.radio("Level", options=["Daycare", "Stay-at Home Parent"])
+
+high_school_graduation_year = st.number_input("High School Graduation Year:", min_value=1900, value=2025)
+education_level = st.radio("Level", options=["High School", "College", "Home Schooling"])
+
+if education_level == "High School":
+    program = st.selectbox("Type of High School", ["Public", "Private"])
+elif education_level == "College":
+    program = st.selectbox("College Program", df["Type"].unique())
+else:
+    program = None
+
+
+program = None
+if "Graduate" in education_level:
+    program = st.selectbox("Graduate Program", options=["Business (MBA)", "Design", "Divinity", "Education", "GSAD", "Government", "Law", "Medical/Dental", "Public Health (1-Year MPH)"])
+
+
+if st.button("Predict Cost"):
+    tuition = estimate_tuition(program, int(high_school_graduation_year), program)
+    new_tuition = reg_loaded.predict() # TODO: need to format data properly
+    st.caption(f"{tuition}")
+
+
+# Note: the prediction doesn't work right now since we need to format the data properly before passing it into the model.
+# Run this script with `streamlit run frontend.py
---
+++ overall_tuition_model.py
@@ -0,0 +1,77 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+import joblib
+
+def loadData():
+   return pd.read_csv("archive/overall_tuition.csv")
+
+def pivotData(df):
+    pivot = (
+        df.pivot_table(
+            index=["Year", "State", "Type", "Length"],   # keys that define one “entry”
+            columns="Expense",
+            values="Value",
+            aggfunc="first",
+            fill_value = 0
+        )
+        .reset_index()
+    )
+
+    pivot["Total_cost"] = pivot["Fees/Tuition"] + pivot["Room/Board"]
+    pivot["Year_scaled"] = pivot["Year"] - 2000
+
+    pivot = pivot.drop(['Year', 'Fees/Tuition', 'Room/Board'], axis=1).copy()
+
+    pivot['Length'] = pivot['Length'].str[0]
+    pivot['Length'] = pivot['Length'].astype(int)
+    return pivot
+
+def removeOutliers(df):
+    Q1 = df.Total_cost.quantile(0.25)
+    Q3 = df.Total_cost.quantile(0.75)
+    IQR = Q3 - Q1
+    df = df.drop(df.loc[df['Total_cost'] > (Q3 + 1.5 * IQR)].index)
+    df = df.drop(df.loc[df['Total_cost'] < (Q1 - 1.5 * IQR)].index)
+    return df
+
+# Drop outlier by IQR calculation
+def encodeData(df):
+    df = pd.get_dummies(df, prefix='Type', columns=['Type'], drop_first=False)
+    df = pd.get_dummies(df, prefix='State', columns=['State'], drop_first=False)
+    return df
+
+df = loadData()
+pivot = pivotData(df)
+pivot = removeOutliers(pivot)
+pivot = encodeData(pivot)
+
+x = pivot.iloc[:, :1].join(pivot.iloc[:, 2:])
+y = pivot.iloc[:,1]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+reg = LinearRegression()
+reg.fit(x_train, y_train)
+
+y_pred = reg.predict(x_test)
+
+r2 = r2_score(y_test, y_pred)
+print("R2 score:", r2)
+print(x_test)
+
+# Testing the model with future years
+test_data = {col: 0 for col in x_train.columns}
+test_data["Year_scaled"] = 25
+test_data["Type_Private"] = 1
+test_data["Type_Public In-State"] = 0
+test_data["Type_Public Out-of-State"] = 0
+test_data["Length"] = 4
+test_df = pd.DataFrame([test_data])
+print(reg.predict(test_df))
+
+joblib.dump(reg, "reg_model.pkl")
+
+# Note: the dataset uses average costs, so predictions are on the lower end
